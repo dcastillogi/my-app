ac3f02967900e7e0ce6af17a4b42cd4925178a17
Daniel Castillo <dev@dcastillogi.com>
2025-05-26 20:51:32 -0500

    la api

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,9 @@
+export const GET = (req) => {
+    
+    return new Response("Hello World!", {
+        status: 200,
+        headers: {
+            "Content-Type": "text/plain"
+        }
+    })
+}
